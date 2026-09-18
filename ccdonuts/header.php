70dbe16e6df608f8cbbb3bb0aa7bd57e2466667c
久我嗣生 <kugashion7829@gmail.com>
2026-09-18 12:23:45 +0900

    Add files via upload

---
+++ ccdonuts/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang=ja>
+    <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./common/reset.css">
+        <link rel="stylesheet" href="./styles/style.css">
+        <title>CCDonut</title>
+    </head>
+    
+    <body>
+        <header>
+            <div class="designWrap headerDesignPrimary">
+                <div class="headerWrapPrimary">
+                    <span></span>
+                    <img src="./images/ccdonutsLogo.svg" alt="ドーナツ屋のロゴ">
+                    
+                    <div class="buyingContainer">
+                        <div class="loginContainer">
+                            <img src="./images/intoArrow.svg" alt="ログイン">
+                            <p>ログイン</p>
+                        </div>
+
+                        <div class="cartContainer">
+                            <img src="./images/cart.svg" alt="カート">
+                            <p>カート</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="designWrap headerDesignSecondary">
+                <div class="headerWrapSecondary">
+                    <form action="" method="post">
+                        <button type="submit" aria-label="検索">
+
+                            <img src="./images/lens.svg" alt="虫眼鏡">
+                        
+                        </button>
+                        <input type="search" id="searchKeyword" name="keyword" aria-label="商品検索">
+                    </form>
+                </div>
+            </div>
+        </header>
+
+        <main>
