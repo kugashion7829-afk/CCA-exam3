f6400548316f27a43b91f4395087a7ee57bb6f22
久我嗣生 <kugashion7829@gmail.com>
2026-09-24 23:11:59 +0900

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,6 +1,22 @@
         </main>
     <footer>
+        <div class=designWrap>
+            <ul>
+                <li><a href="#">よくある質問</a></li>
+                <li><a href="#">問い合わせ</a></li>
+                <li><a href="#">当サイトのポリシー</a></li>
+            </ul>
+            
+            <img src="./images/ccdonutsLogo.svg" alt="ドーナツ屋のロゴ">
+            <div class="SNS">
+                <a href="#"><img src="./images/Facebook.svg" alt="Facebook"></a>
+                <a href="#"><img src="./images/Instagram.svg" alt="Instagram"></a>
+                <a href="#"><img src="./images/X.svg" alt="エックス"></a>
+            </div>
 
+            <hr>
+            <small>Copyright © 2026 c.c.donuts</small>
+        </div>
     </footer>
     <script src="./scripts/script.js"></script>
     </body>
